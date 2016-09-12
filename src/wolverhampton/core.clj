dc211a553442e4ae8dba5d617decf7b74282a3a2
Dave Bush <github@david-bush.co.uk>
2016-09-12 11:13:51 +0700

    Added lookahead; printed output.

--- src/wolverhampton/core.clj
+++ src/wolverhampton/core.clj
@@ -7,8 +7,11 @@
 
 (defn get-data [html]
   "Pulls out meta-data for function"
-  (read-string (clojure.string/replace (second (re-find #"PAGE_DATA=\"(.*)\";" html)) #"\\" "")))
+  (read-string (clojure.string/replace (second (re-find #"PAGE_DATA=\"(.*)\";" html)) #"\\(?!n)" "")))
+
+(defn get-examples []
+  (doseq [e (:examples (get-data (get-html)))] (println (:body e))))
 
 (defn -main
   [& args]
-  (println (:var (get-data (get-html)))))
+  (println (get-examples)))
